2ad367cfae668d587e02d5fd7d7d14cbc8d20a5a
Davide Cristini <davcri91@gmail.com>
2025-06-20 23:54:43 +0200

    chore: remove unneeded code.
    reparent() is now provided by Godot Engine API

--- addons/ggt-core/ggt-core.gd
+++ addons/ggt-core/ggt-core.gd
@@ -10,7 +10,6 @@ func _enable_plugin():
 	add_autoload_singleton("Game", "res://addons/ggt-core/game.gd")
 	add_autoload_singleton("Transitions", "res://addons/ggt-core/transitions/transitions.tscn")
 	add_autoload_singleton("Scenes", "res://addons/ggt-core/scenes/scenes.gd")
-	add_autoload_singleton("Utils", "res://addons/ggt-core/utils/utils.gd")
 
 
 func _exit_tree():
